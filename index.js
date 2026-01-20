8d1651d5d32d367bacc4f6ebe64e3ae415679566
muthuc9pro2-code <muthuc9pro2@gmail.com>
2026-01-20 17:24:23 +0530

    changed

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-hi there
-console.log("Hello, world!");
+const react1 = {
+    width: 10,
+    height: 20
+}
+
+function cal(react) {
+    return react.width * react.height;
+}
+
+const ans = cal(react1);
+console.log(ans);
+
+
+
