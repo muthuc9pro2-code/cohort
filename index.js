7241aac7c52e478e0e54179eaeda4b3e4e251b0e
muthuc9pro2-code <muthuc9pro2@gmail.com>
2026-01-23 17:13:31 +0530

    changed

--- index.js
+++ index.js
@@ -16,6 +16,12 @@ ans = new rectangle ( 5,8, "red");
 area = ans .area() ;
 ans. paint ();
 console.log (area);
-    
+
+function  sum (a,b){
+    return a + b;
+ }
+    ans = sum ( 12576, 25634);
+    console.log (ans);
+
 
 
