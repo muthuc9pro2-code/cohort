0cdb4ebb662dd0f5e8790e2f9db5984862bbb8fa
muthu kumaaran <muhtuc9pro2@gamil.com>
2026-01-29 10:01:22 +0000

    change

--- index.js
+++ index.js
@@ -1,24 +1,4 @@
-function test () {
-    return new Promise((resolve) => {
-        c = 0
-        for (let i = 0 ; i < 100000 ; i++){
-            c++;
-        }
-        resolve("poda podalanga");
-    });
-}
-let p = test()
 
-function callback (display) {
-    console.log(display);
-}
-
-p.then (callback)
-
-function sum(a, b) {
-    return a+b;
-}
-console.log(sum(2,4));
 
 
 
