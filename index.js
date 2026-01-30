61c3585072ff043d718b3760e0806c350adacab6
muthuc9pro2-code <muthuc9pro2@gmail.com>
2026-01-30 10:39:34 +0530

    changed

--- index.js
+++ index.js
@@ -1,17 +1,4 @@
-function inAnagram(str1, str2) {
-     sortedstr1 = str1.toLowerCase().split("").sort().join("");
-     sortedstr2 = str2.toLowerCase().split("").sort().join("");
 
-    if (sortedstr1 == sortedstr2) {
-        console.log(true);
-        return true;
-    } else {
-        console.log(false);
-        return false;
-    }
-}
-
-inAnagram("MUThukumar", "all the best"); 
 
 
 
