43855f104cddd827e8c3cc42f7f8013e93277e91
muthuc9pro2-code <muthuc9pro2@gmail.com>
2026-02-13 15:44:17 +0530

    changed

--- todo.js
+++ todo.js
@@ -1,3 +1,50 @@
-function  update(event) {
-    event.target.remove()
+function update() {
+    document.querySelector(".outputform").style.display = "block";
+
+    
+         
+    
+    var name = document.getElementById("name").value;
+    var age = document.getElementById("age").value;
+    var course = document.getElementById("course").value;
+    var email = document.getElementById("email").value;
+
+    var genderElements = document.getElementsByName("gender");
+    var gender = "";
+
+    for (var i = 0; i < genderElements.length; i++) {
+        if (genderElements[i].checked) {
+            gender = genderElements[i].value;
+        }
+    }
+
+   
+    var row = document.createElement("tr");
+
+    row.innerHTML = `
+        <td>${name}</td>
+        <td>${age}</td>
+        <td>${course}</td>
+        <td>${gender}</td>
+        <td>${email}</td>
+        <td><button style = bac onclick="deleteRow(this)">Delete</button></td>
+    `;
+
+    document.getElementById("outputprint").appendChild(row);
+    
 }
+
+function deleteRow(button) {
+    button.parentElement.parentElement.remove();
+    let a = document.getElementById("outputprint");
+    if (a.children.length === 0 ) {
+         document.querySelector(".outputform").style.display = "none";
+
+    }
+    }
+
+   
+
+
+
+
